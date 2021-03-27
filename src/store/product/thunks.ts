1d169dc9c72eb270400dba496a3f339112b9fc3b
kyleatblackfoot <kyle@blackfoot.net.nz>
2021-03-28 12:34:39 +1300

    update standalone support

--- src/store/product/thunks.ts
+++ src/store/product/thunks.ts
@@ -5,15 +5,18 @@ import {productAddMaterials, productAddModelData, productAddCartVariationIds} fr
 import {shapeTextureData} from '../../utils';
 import pathConfig from '../../config/pathConfig';
 
-const mockProductData = process.env.STANDALONE ? require('../../mock/product.json') : null;
-const mockTextureData = process.env.STANDALONE ? require('../../mock/texture.json') : null;
+const IS_STANDALONE = process.env.STANDALONE;
+const IS_DEVELOPMENT = process.env.NODE_ENV === 'development';
 
-const PRODUCT_ID = process.env.NODE_ENV === 'development' ? '51' : document.getElementById('post_id')?.dataset.id;
+const mockProductData = IS_STANDALONE ? require('../../mock/product.json') : null;
+const mockTextureData = IS_STANDALONE ? require('../../mock/texture.json') : null;
+
+const PRODUCT_ID = IS_DEVELOPMENT ? '51' : document.getElementById('post_id')?.dataset.id;
 
 export const thunkProductLoadMaterials = (): ThunkAction<void, RootState, unknown, Action<string>> => async(dispatch: Dispatch, getState: GetState) => {
-    fetch(pathConfig.endpoints.texture)
+    fetch(IS_STANDALONE ? `https://jsonplaceholder.typicode.com/todos/100` : pathConfig.endpoints.texture)
     .then(res => res.json())
-    .then(data => process.env.STANDALONE ? shapeTextureData(mockTextureData) : shapeTextureData(data))
+    .then(data => IS_STANDALONE ? shapeTextureData(mockTextureData) : shapeTextureData(data))
     .then(textures => {
         dispatch(productAddMaterials([{
             uid: -2,
@@ -30,9 +33,9 @@ export const thunkProductLoadMaterials = (): ThunkAction<void, RootState, unknow
 }
 
 export const thunkProductLoadModel = (): ThunkAction<void, RootState, unknown, Action<string>> => async(dispatch: Dispatch, getState: GetState) => {
-    fetch(`${pathConfig.endpoints.product}/${PRODUCT_ID}`)
+    fetch(IS_STANDALONE ? `https://jsonplaceholder.typicode.com/todos/100` : `${pathConfig.endpoints.product}/${PRODUCT_ID}`)
     .then(res => res.json())
-    .then(data => process.env.STANDALONE ? dispatch(productAddModelData(mockProductData)) : dispatch(productAddModelData(data)))
+    .then(data => IS_STANDALONE ? dispatch(productAddModelData(mockProductData)) : dispatch(productAddModelData(data)))
     .catch(error => console.error('thunkProductLoadModel was not successful', error));
 }
 
